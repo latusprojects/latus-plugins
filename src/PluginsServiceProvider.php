385bfd319bb82649e9dcfd8edea36c2861ca6270
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-06-28 21:36:23 +0200

    Add composer.json and PluginsServiceProvider.php

---
+++ src/PluginsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Latus\Plugins;
+
+use Illuminate\Support\ServiceProvider;
+
+class PluginsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+}
