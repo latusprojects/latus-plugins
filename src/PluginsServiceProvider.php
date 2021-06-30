e2d82c6e829f38628a6b59dc0b2e5bd6008ec550
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-06-30 17:36:58 +0200

    Reformat files and add 'latusprojects/latus-model-repositories' package to dependencies

--- src/PluginsServiceProvider.php
+++ src/PluginsServiceProvider.php
@@ -13,7 +13,7 @@ class PluginsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        
     }
 
     /**
@@ -23,6 +23,6 @@ class PluginsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
