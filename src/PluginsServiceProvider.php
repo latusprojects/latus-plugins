41dd3941a57010bcd91a47137f7b38bfd0eb52c2
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-16 02:39:33 +0200

    Add bindings for ComposerRepositoryRepository.php and ThemeRepository.php contracts

--- src/PluginsServiceProvider.php
+++ src/PluginsServiceProvider.php
@@ -3,9 +3,12 @@
 namespace Latus\Plugins;
 
 use Illuminate\Support\ServiceProvider;
-use Latus\Plugins\Models\Plugin;
 use Latus\Plugins\Repositories\Contracts\PluginRepository as PluginRepositoryContract;
 use Latus\Plugins\Repositories\Eloquent\PluginRepository;
+use Latus\Plugins\Repositories\Contracts\ComposerRepositoryRepository as ComposerRepositoryRepositoryContract;
+use Latus\Plugins\Repositories\Eloquent\ComposerRepositoryRepository;
+use Latus\Plugins\Repositories\Contracts\ThemeRepository as ThemeRepositoryContract;
+use Latus\Plugins\Repositories\Eloquent\ThemeRepository;
 
 class PluginsServiceProvider extends ServiceProvider
 {
@@ -16,12 +19,17 @@ class PluginsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (!$this->app->bound(Plugin::class)) {
-            $this->app->bind(Plugin::class, Plugin::class);
-        }
 
         if (!$this->app->bound(PluginRepositoryContract::class)) {
             $this->app->bind(PluginRepositoryContract::class, PluginRepository::class);
+        }
+
+        if (!$this->app->bound(ComposerRepositoryRepositoryContract::class)) {
+            $this->app->bind(ComposerRepositoryRepositoryContract::class, ComposerRepositoryRepository::class);
+        }
+
+        if (!$this->app->bound(ThemeRepositoryContract::class)) {
+            $this->app->bind(ThemeRepositoryContract::class, ThemeRepository::class);
         }
 
         $this->mergeConfigFrom(__DIR__ . '/../config/latus-plugins.php', 'latus-plugins');
