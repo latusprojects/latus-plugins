43b326db762ec9534552926b967d18147f597796
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-16 02:30:38 +0200

    Add ComposerRepositoryRepository.php contract and eloquent repository

---
+++ src/Repositories/Eloquent/ComposerRepositoryRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace Latus\Plugins\Repositories\Eloquent;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Latus\Plugins\Models\ComposerRepository;
+use Latus\Plugins\Repositories\Contracts\ComposerRepositoryRepository as ComposerRepositoryRepositoryContract;
+use Latus\Repositories\EloquentRepository;
+
+class ComposerRepositoryRepository extends EloquentRepository implements ComposerRepositoryRepositoryContract
+{
+
+    public function __construct(ComposerRepository $composerRepository)
+    {
+        parent::__construct($composerRepository);
+    }
+
+    public function activate(ComposerRepository $composerRepository): void
+    {
+        $composerRepository->status = ComposerRepository::STATUS_ACTIVATED;
+        $composerRepository->save();
+    }
+
+    public function deactivate(ComposerRepository $composerRepository): void
+    {
+        $composerRepository->status = ComposerRepository::STATUS_DEACTIVATED;
+        $composerRepository->save();
+    }
+
+    public function delete(ComposerRepository $composerRepository)
+    {
+        $composerRepository->delete();
+    }
+
+    public function getName(ComposerRepository $composerRepository): string
+    {
+        return $composerRepository->name;
+    }
+
+    public function getAllActive(): Collection
+    {
+        return ComposerRepository::where('status', ComposerRepository::STATUS_ACTIVATED)->get();
+    }
+
+    public function findByName(string $name): Model|null
+    {
+        return ComposerRepository::where('name', $name)->first();
+    }
+
+    public function findByUrl(string|null $url): Model|null
+    {
+        return ComposerRepository::where('url', $url)->first();
+    }
+}
